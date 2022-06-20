6c933253d2b04d452d0e723a2b3bcdd019d9678b
Olena <e2195610@cmaisonneuve.qc.ca>
2022-06-20 10:18:37 -0400

    Première  structure de thème

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+ /**
+  * Template header
+  * Ve template est intégré dans tous les modèles de thème
+  * @link : https://developer.wordpress.org/themes/
+  * @package : 31w
+  */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>  >
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <section class="site">
+        <header class="site__header">
+            <section class="site__branding">
+                <h1>
+                    <a href="<?= esc_url( home_url( '/' )) ?>"><?= bloginfo( 'name' ); ?></a>
+                </h1>
+                <h2>
+                <?= bloginfo( 'description' ); ?>
+                </h2>
+            </section>
+            <nav class="site__navigation">
+            Menu
+            </nav>
+        </header>
+    
+
