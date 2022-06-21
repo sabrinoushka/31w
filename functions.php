093911fff5ca1dfd4e091ca231b1d09e0192ec6e
Olena <e2195610@cmaisonneuve.qc.ca>
2022-06-20 23:47:03 -0400

    Ajout de la fonction 'mon_31w_enqueue'

--- functions.php
+++ functions.php
@@ -11,7 +11,11 @@ function mon_31w_setup() {
 } 
 add_action( 'after_setup_theme', 'mon_31w_setup' );
 
-
-
-
+function mon_31w_enqueue() {
+	wp_enqueue_style( 'mon_31w-style', 
+						get_stylesheet_uri(),
+						array(), 
+						_S_VERSION );	
+}
+add_action( 'wp_enqueue_scripts', 'mon_31w_enqueue' );
 ?>
