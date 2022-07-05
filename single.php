d141301303435a60dc05d270e20f36432831b1f2
Olena <e2195610@cmaisonneuve.qc.ca>
2022-07-04 21:22:36 -0400

    Modification du 'style.css'

--- single.php
+++ single.php
@@ -1,20 +1,21 @@
 <?php get_header(); ?> 
-<h1>Mon premier modèle Wordpress</h1>
-<?php
-if (have_posts()):
-    while(have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
-        <?php
-          $titre = get_the_title();
-          $sigle = substr($titre,0,8);
-          $heure = substr($titre,-6);
+<section class="site__main">
+  <h1>Mon premier modèle Wordpress</h1>
+  <?php
+  if (have_posts()):
+      while(have_posts()) : the_post(); ?>
+          <h2><?php the_title(); ?></h2>
+          <p><?php the_content(); ?></p>
+          <?php
+            $titre = get_the_title();
+            $sigle = substr($titre,0,8);
+            $heure = substr($titre,-6);
 
-        ?>
-        <code><?= $sigle ?></code>
-        <code><?= $heure ?></code>
+          ?>
+          <code><?= $sigle ?></code>
+          <code><?= $heure ?></code>
 
-    <?php endwhile; ?>
-<?php endif; ?>    
-
+      <?php endwhile; ?>
+  <?php endif; ?>    
+  </section>
 <?php get_footer(); ?>
